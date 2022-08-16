fc099cff3317e14412327db2c23fd920a899d4d2
JB-Tellez <jb.tellez@gmail.com>
2022-08-16 09:37:16 -0700

    passes test but has bad formatting

--- test_main.py
+++ test_main.py
@@ -1,13 +1,11 @@
 from main import say_hello
 
-
 def test_default():
     actual = say_hello()
     expected = "Hello stranger"
     assert actual == expected
 
-
 def test_with_arg():
     actual = say_hello("neighbor")
-    expected = "fail on purpose"
+    expected = "Hello neighbor"
     assert actual == expected
