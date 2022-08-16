7102c44398377a15cca883a5acb866a0c1b09be1
JB-Tellez <jb.tellez@gmail.com>
2022-08-16 09:45:02 -0700

    includes flake8, one badly formatted file

--- test_main.py
+++ test_main.py
@@ -1,9 +1,11 @@
 from main import say_hello
 
+
 def test_default():
     actual = say_hello()
     expected = "Hello stranger"
     assert actual == expected
+
 
 def test_with_arg():
     actual = say_hello("neighbor")
